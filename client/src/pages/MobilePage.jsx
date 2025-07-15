e711b74f887eefee560ec44174f15d8d80de9b86
bandikarvijay <bandikar.v.vijay@gmail.com>
2025-07-15 22:14:53 +0530

    Update MobilePage.jsx

--- client/src/pages/MobilePage.jsx
+++ client/src/pages/MobilePage.jsx
@@ -9,7 +9,7 @@ const MobilePage = () => {
   const [selectedImage, setSelectedImage] = useState(null);
 
   const fetchProjects = async () => {
-    const res = await axios.get('/api/projects?category=Mobile');
+    const res = await axios.get('https://project-drop-backend.onrender.com/api/projects?category=Mobile');
     setProjects(res.data);
   };
 
