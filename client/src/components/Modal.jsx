d3ea30697c853208ee7e877945168562c0794281
bandikarvijay <bandikar.v.vijay@gmail.com>
2025-07-16 11:31:56 +0530

    Update Modal.jsx

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -3,11 +3,8 @@ import './Modal.css';
 
 const Modal = ({ image, onClose }) => {
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={e => e.stopPropagation()}>
-        <img src={image} alt="Fullscreen" className="fullscreen-image" />
-        <button onClick={onClose} className="close-button">✖</button>
-      </div>
+    <div className="modal-backdrop" onClick={onClose}>
+      <img src={image} alt="Full View" className="modal-image" />
     </div>
   );
 };
